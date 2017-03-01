49525f83bc27450246f0427415ed8e41e05c558b
RafM <rkjmosiolek@gmail.com>
2017-03-01 11:45:06 +0100

    Solution to the fourth problem added

--- projecteuler.js
+++ projecteuler.js
@@ -105,6 +105,39 @@ function problemThree(value) {
 problemThree(13195); // Output: The largest prime factor of this number is: 29
 problemThree(600851475143); // Output: The largest prime factor of this number is: 6857
 
+/*
+
+Problem 4.
+
+Largest palindrome product.
+
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+
+*/
 
 
+function problemFour() {
+  
+  var reversedString = function(value) {            // function that reverse a string with joint build-in functions: split(), reverse() and join().
+    return value.split("").reverse().join("")
+};
+var x = y = 0;
+var arr = [];
+
+for (x = 100; x <= 999; x++) {
+    for (y = 100; y <=999; y++) {
+      
+      var i = x * y;
+      var string = i.toString();                    // .toString() - build-in function that change any object into a string
+      if (string === reversedString(string)) {
+        arr.push(i);
+    }
+}
+}
+  return Math.max(...arr);                          // The Math.max() function returns the largest of zero or more numbers; using spread operator
+}
+
+problemFour(); // Output: 906609.
 
