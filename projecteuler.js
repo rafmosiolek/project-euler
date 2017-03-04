4a688467afe3bb27de13f3a459d5adf4a8da8403
RafM <rkjmosiolek@gmail.com>
2017-03-04 21:43:07 +0100

    Solution to problem 6 added

--- projecteuler.js
+++ projecteuler.js
@@ -176,3 +176,36 @@ function problemFive(value) {
  
 problemFive(20);
 
+
+
+/* Problem 6.
+
+Sum square difference
+Problem 6
+The sum of the squares of the first ten natural numbers is,
+
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+*/
+
+function problemSix(n) {
+
+  var sum = sumOfSquares = 0;
+  
+  for (var i=1; i<=n; i++) {
+  sumOfSquares += Math.pow(i,2);
+}
+  for (var j=1; j<=n; j++) {
+  sum += j;
+}
+  var squareOfSum = Math.pow(sum,2);
+  return squareOfSum - sumOfSquares;
+}
+
+problemSix(100); //Output: 25164150.
